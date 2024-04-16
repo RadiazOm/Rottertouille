d8c6dbc5c52e2b50e972494fb64e0049fe137082
Huseyin Furkan Ozcelik <furkanzelik@gmail.com>
2024-04-16 10:47:54 +0200

    commit to pull

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ReceptenTonen from "./receptenTonen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app !</Text>
+        <StatusBar style="auto"/>
+        <ReceptenTonen/>
+      </View>
+
   );
 }
 
